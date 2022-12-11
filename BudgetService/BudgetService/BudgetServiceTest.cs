126c4f20eff03f8c68c6913660d4e2509b0e7303
alomarliu <alomarliu@yile.com.tw>
2022-12-11 08:57:04 +0800

    fix exception

--- BudgetService/BudgetService/BudgetServiceTest.cs
+++ BudgetService/BudgetService/BudgetServiceTest.cs
@@ -80,5 +80,26 @@ namespace BudgetService
 
             Assert.AreEqual(138, budgetService.Query(startTime, endTime));
         }
+
+
+        [TestMethod]
+        public void none_data_query()
+        {
+            //new Budget() { yearMonth = "202201", amount = 310 },
+            //new Budget() { yearMonth = "202202", amount = 28 },
+            //new Budget() { yearMonth = "202203", amount = 3100 },
+            //new Budget() { yearMonth = "202205", amount = 31 },
+            //new Budget() { yearMonth = "202206", amount = 300 },
+            //new Budget() { yearMonth = "202207", amount = 310 },
+            //new Budget() { yearMonth = "202208", amount = 0 },
+            //new Budget() { yearMonth = "202209", amount = 300 },
+            var startTime = new DateTime(2022, 2, 28);
+            var endTime = new DateTime(2022, 4, 1);
+
+            var dbRepo = new DBRepo();
+            var budgetService = new BudgetService(dbRepo);
+
+            Assert.AreEqual(3101, budgetService.Query(startTime, endTime));
+        }
     }
 }
